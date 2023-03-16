68ecadd4ea0dd7f170cd7d65e3d377c162a5bcfb
pankazakos <pankazakos01@gmail.com>
2023-03-16 21:30:57 +0200

    fixed CRA import error

--- react/auction/src/index.js
+++ react/auction/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import '../node_modules/@fontawesome/fontawesome-free/css/all.min.css';
+import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
 import './favicon.ico';
