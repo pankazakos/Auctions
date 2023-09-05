94a301abd9588ceacff41b3218e94371810537b4
Panagiotis Kazakos <pankazakos01@gmail.com>
2023-09-05 10:57:26 +0300

    fixed db connection error

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,13 @@
 FROM python:3.10.4
+
 ENV PYTHONDONTWRITEBYTECODE=1
+
 ENV PYTHONUNBUFFERED=1
+
 WORKDIR /code
+
 COPY requirements.txt /code/
-RUN pip install -r requirements.txt
+
+RUN pip install --upgrade pip && pip install -r requirements.txt
+
 COPY . /code/
--- auction/settings.py
+++ auction/settings.py
@@ -83,12 +83,12 @@ WSGI_APPLICATION = 'auction.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'db',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME', 'auctions'),
+        'USER': os.environ.get('DB_USER', 'postgres'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'postgres'),
+        'HOST': os.environ.get('DB_HOST', 'db'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 
